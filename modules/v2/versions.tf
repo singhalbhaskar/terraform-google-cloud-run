9b00c3f77cfe7d7c2e3b310ba44a367db9895357
singhalbhaskar <singhalbhaskar@google.com>
2026-02-05 10:54:32 +0530

    Update versions.tf

--- modules/v2/versions.tf
+++ modules/v2/versions.tf
@@ -18,8 +18,8 @@ terraform {
   required_version = ">= 1.3"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
+    google-beta = {
+      source  = "hashicorp/google-beta"
       version = ">= 6, < 8"
     }
   }
