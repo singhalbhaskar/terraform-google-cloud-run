dace7222e00b3f6c3bca148dffb06e9eef5a18ed
singhalbhaskar <singhalbhaskar@google.com>
2026-02-12 15:03:27 +0530

    Update versions.tf

--- modules/v2/versions.tf
+++ modules/v2/versions.tf
@@ -18,21 +18,9 @@ terraform {
   required_version = ">= 1.3"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 6, < 8"
-    }
     google-beta = {
       source  = "hashicorp/google-beta"
       version = ">= 6, < 8"
     }
-  }
-
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v0.25.0"
-  }
-
-  provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-cloud-run:v2/v0.25.0"
   }
 }
